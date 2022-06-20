0c338217b4bb66dd066931bc5f8d8a95fc02180c
Tamim Mahmud <internationalinf24@gmail.com>
2022-06-20 22:21:40 +0600

    Bigger and Rich calculation in js

---
+++ index.js
@@ -0,0 +1,39 @@
+var business = 1250;
+var minister = 458;
+var sochib = 6150;
+if (business> minister){
+    if (business > sochib){
+        console.log("Business is bigger");
+    }
+    else {
+        console.log("Sochib is bigger");
+    }
+}
+else{
+    if(minister > sochib){
+        console.log("minister is bigger");
+    }
+    else{
+        console.log("sochib is bigger");
+    }
+}
+
+var borovai = 2500;
+var mejhovai = 1500;
+var sotovai = 850;
+if(borovai > mejhovai){
+    if(borovai > sotovai){
+        console.log("Borovai is rich");
+    }
+    else{
+        console.log("Sotovai is rich");
+    }
+}
+else{
+    if(mejhovai > sotovai){
+        console.log("Mejhovai is rich");
+    }
+    else{
+        console.log("Sotovai is Rich");
+    }
+}
